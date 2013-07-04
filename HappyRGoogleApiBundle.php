12715fa5fa6a3704bd3cda9a55d6949434823c61
Nyholm <tobias.nyholm@gmail.com>
2013-07-04 14:09:18 +0200

    Minor fixes

--- HappyRGoogleApiBundle.php
+++ HappyRGoogleApiBundle.php
@@ -7,7 +7,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 /**
  * Class HappyRGoogleApiBundle
  *
- * This bundle is a symfony wrapper for the google php api
+ * This bundle is a symfony wrapper for the google php api.
  */
 class HappyRGoogleApiBundle extends Bundle
 {
