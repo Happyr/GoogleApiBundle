48257de8ec66ebbc6cccf6b708f799f9f4387e63
Nyholm <tobias.nyholm@gmail.com>
2013-07-07 15:48:24 +0200

    added dummy test

---
+++ Tests/Services/GoogleClientTest.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace HappyR\Google\ApiBundle\Tests\Services;
+
+
+class GoogleClientTest extends \PHPUnit_Framework_TestCase
+{
+    public function testNothing()
+    {
+        $this->assertTrue(true);
+    }
+}
