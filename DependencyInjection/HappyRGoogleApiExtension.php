55804e9a2c68e771eb4ac36313e5567862617327
Andreas Schempp <andreas.schempp@terminal42.ch>
2015-07-30 16:45:47 +0200

    Use ConfigurableExtension class provided by Symfony
    see http://symfony.com/doc/current/cookbook/bundles/configuration.html

--- DependencyInjection/HappyRGoogleApiExtension.php
+++ DependencyInjection/HappyRGoogleApiExtension.php
@@ -4,7 +4,7 @@ namespace HappyR\Google\ApiBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\HttpKernel\DependencyInjection\ConfigurableExtension;
 use Symfony\Component\DependencyInjection\Loader;
 
 /**
@@ -12,16 +12,13 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class HappyRGoogleApiExtension extends Extension
+class HappyRGoogleApiExtension extends ConfigurableExtension
 {
     /**
      * {@inheritDoc}
      */
-    public function load(array $configs, ContainerBuilder $container)
+    public function loadInternal(array $config, ContainerBuilder $container)
     {
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
-
         $container->setParameter('happy_r_google_api', $config);
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
